22e25c4b4341567c4853b2f11a149de079113c17
mochi1618 <132237132+mochibird@users.noreply.github.com>
2023-07-10 16:21:50 +0900

    Create test.php

---
+++ dokugakuHitAndBlow/test.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once(__DIR__ . '/../lib/HitAndBlow.php');
+
+class HitAndBlowTest extends TestCase
+{
+    public function testJudge()
+    {
+        $this->assertSame([4, 0], judge(5678, 5678));
+        $this->assertSame([1, 1], judge(5678, 7612));
+        $this->assertSame([0, 4], judge(5678, 8756));
+        $this->assertSame([0, 0], judge(5678, 1234));
+    }
+}
